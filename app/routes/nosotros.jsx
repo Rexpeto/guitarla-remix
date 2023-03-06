dcab00ca58133b899f4c886caf4c3d3729a5c565
Carlos Gallardo <Carlosgallardoparra@hotmail.com>
2023-03-06 18:10:06 -0400

    Ruta y página de nosotros

---
+++ app/routes/nosotros.jsx
@@ -0,0 +1,14 @@
+export function meta() {
+  return (
+    {
+      title: "GuitarLa - Nosotros"
+    }
+  )
+}
+
+function Nosotros() {
+  return (
+    <div>Nosotros</div>
+  )
+}
+export default Nosotros
