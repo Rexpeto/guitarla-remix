208cfc6da695c1fe9b74f2a16df5f110a8ecd082
Carlos Gallardo <Carlosgallardoparra@hotmail.com>
2023-03-06 18:09:19 -0400

    Utilización de Navegación

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,47 +1,14 @@
-import { Link, useLocation } from "@remix-run/react";
+import Navegacion from "./navegacion";
 import logo from "../../public/img/logo.svg";
 
 function Header() {
-    const location = useLocation();
-    console.log(location);
     return (
         <header className="header">
             <div className="contenedor barra">
                 <div className="logo">
                     <img src={logo} alt="Logo" />
                 </div>
-                <nav className="navegacion">
-                    <Link
-                        to="/"
-                        className={location.pathname === "/" ? "active" : ""}
-                    >
-                        Inicio
-                    </Link>
-                    <Link
-                        to="/blog"
-                        className={
-                            location.pathname === "/blog" ? "active" : ""
-                        }
-                    >
-                        Blog
-                    </Link>
-                    <Link
-                        to="/nosotros"
-                        className={
-                            location.pathname === "/nosotros" ? "active" : ""
-                        }
-                    >
-                        Nosotros
-                    </Link>
-                    <Link
-                        to="/tienda"
-                        className={
-                            location.pathname === "/tienda" ? "active" : ""
-                        }
-                    >
-                        Tienda
-                    </Link>
-                </nav>
+                <Navegacion />
             </div>
         </header>
     );
