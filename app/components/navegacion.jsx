2050aa99d31d3e106bf78842ea73d9b8e4e08e82
Carlos Gallardo <Carlosgallardoparra@hotmail.com>
2023-03-06 18:08:54 -0400

    Componente de navegación

---
+++ app/components/navegacion.jsx
@@ -0,0 +1,33 @@
+import { Link, useLocation } from "@remix-run/react";
+
+const Navegacion = () => {
+    const location = useLocation();
+
+    return (
+        <nav className="navegacion">
+            <Link to="/" className={location.pathname === "/" ? "active" : ""}>
+                Inicio
+            </Link>
+            <Link
+                to="/blog"
+                className={location.pathname === "/blog" ? "active" : ""}
+            >
+                Blog
+            </Link>
+            <Link
+                to="/nosotros"
+                className={location.pathname === "/nosotros" ? "active" : ""}
+            >
+                Nosotros
+            </Link>
+            <Link
+                to="/tienda"
+                className={location.pathname === "/tienda" ? "active" : ""}
+            >
+                Tienda
+            </Link>
+        </nav>
+    );
+};
+
+export default Navegacion;
