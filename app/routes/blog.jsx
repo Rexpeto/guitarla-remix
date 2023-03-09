9e6a1603f11ee95d6a49b35f5f23ce167376cd18
Carlos Gallardo <Carlosgallardoparra@hotmail.com>
2023-03-09 01:22:20 -0400

    Mostrando posts

--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,6 +1,25 @@
+import { useLoaderData } from "@remix-run/react";
+import { getPosts } from "~/models/blog.server";
+import Post from "~/components/post";
+
+export const loader = async () => {
+    const posts = await getPosts();
+
+    return posts;
+};
+
 function Blog() {
+    const posts = useLoaderData();
+
     return (
-        <div>Blog</div>
+        <div className="contnedor">
+            <h2 className="heading">Blog</h2>
+            <div className="blog">
+                {posts.map((post) => (
+                    <Post key={post?.id} post={post?.attributes} />
+                ))}
+            </div>
+        </div>
     );
 }
 export default Blog;
