0a413a27f437670b4e1d5c6f09d578ed04dd767b
Carlos Gallardo <Carlosgallardoparra@hotmail.com>
2023-03-09 22:55:03 -0400

    Obtener entrada de blog

--- app/models/blog.server.js
+++ app/models/blog.server.js
@@ -10,3 +10,16 @@ export const getPosts = async () => {
         console.log(error);
     }
 };
+
+export const getPost = async (url) => {
+    try {
+        const respuesta = await fetch(
+            `${process.env.API_URL}/posts?filters[url]=${url}&populate=imagen`
+        );
+        const resultado = await respuesta.json();
+
+        return resultado?.data[0]?.attributes;
+    } catch (error) {
+        console.log(error);
+    }
+};
