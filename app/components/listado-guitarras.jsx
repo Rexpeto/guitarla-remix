905618c4da17c08643c8f8e4be56252be6535af9
Carlos Gallardo <Carlosgallardoparra@hotmail.com>
2023-03-11 12:27:50 -0400

    Lista de guitarras

---
+++ app/components/listado-guitarras.jsx
@@ -0,0 +1,21 @@
+import Guitarra from "~/components/guitarra";
+
+const ListadoGuitarras = ({ guitarras }) => {
+    return (
+        <>
+            <h2 className="heading">Nuestra Colección</h2>
+            {guitarras?.length && (
+                <div className="guitarras-grid">
+                    {guitarras.map((guitarra) => (
+                        <Guitarra
+                            key={guitarra?.id}
+                            guitarra={guitarra?.attributes}
+                        />
+                    ))}
+                </div>
+            )}
+        </>
+    );
+};
+
+export default ListadoGuitarras;
