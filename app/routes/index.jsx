47d25fdbc3570ab61345ebad7cfe05f262b0c8ee
Carlos Gallardo <Carlosgallardoparra@hotmail.com>
2023-03-11 12:27:32 -0400

    Mostrando las guitarras

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,6 +1,43 @@
+import { useLoaderData } from "@remix-run/react";
+import { getGuitarras } from "~/models/guitarras.server";
+import { getPosts } from "~/models/blog.server";
+import ListadoGuitarras from "~/components/listado-guitarras";
+import stylesGuitarra from "~/styles/guitarras.css";
+
+export const meta = () => {
+    return {
+        title: "GuitarLa",
+        description: "GuitarLa ventas de guitarras de mejor calidad",
+    };
+};
+
+export const links = () => {
+    return [
+        {
+            rel: "stylesheet",
+            href: stylesGuitarra,
+        },
+    ];
+};
+
+export const loader = async () => {
+    const [guitarras, posts] = await Promise.all([getGuitarras(), getPosts()]);
+
+    return {
+        guitarras,
+        posts,
+    };
+};
+
 function Index() {
-  return (
-    <h1>Desde index</h1>
-  )
+    const { guitarras, posts } = useLoaderData();
+
+    return (
+        <>
+            <main className="contenedor">
+                <ListadoGuitarras guitarras={guitarras} />
+            </main>
+        </>
+    );
 }
-export default Index
+export default Index;
