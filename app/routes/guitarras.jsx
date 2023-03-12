4b72537637408466230be04bf17915071abc293f
Carlos Gallardo <Carlosgallardoparra@hotmail.com>
2023-03-12 10:58:05 -0400

    Layout de tienda

---
+++ app/routes/guitarras.jsx
@@ -0,0 +1,16 @@
+import { useLoaderData, Outlet } from "@remix-run/react";
+import styleGuitarra from "~/styles/guitarras.css";
+
+export function links() {
+    return [
+        {
+            rel: "stylesheet",
+            href: styleGuitarra,
+        },
+    ];
+}
+
+function Tienda() {
+    return <Outlet />;
+}
+export default Tienda;
