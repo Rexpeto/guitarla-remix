b0abd3962b9432e0df234a220c32dcc6a818dea5
Carlos Gallardo <Carlosgallardoparra@hotmail.com>
2023-03-12 11:15:07 -0400

    Layout de blog

--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,20 +1,5 @@
-import { useLoaderData } from "@remix-run/react";
-import { getPosts } from "~/models/blog.server";
-import Post from "~/components/post";
+import { Outlet } from "@remix-run/react";
 import style from "~/styles/blog.css";
-
-export const loader = async () => {
-    const posts = await getPosts();
-
-    return posts;
-};
-
-export const meta = () => {
-    return {
-        title: "GuitarLa - Blog",
-        descripcion: "GuitarLa, venta de guitarras de la mejor calidad",
-    };
-};
 
 export const links = () => {
     return [
@@ -26,17 +11,6 @@ export const links = () => {
 };
 
 function Blog() {
-    const posts = useLoaderData();
-
-    return (
-        <div className="contenedor">
-            <h2 className="heading">Blog</h2>
-            <div className="blog">
-                {posts.map((post) => (
-                    <Post key={post?.id} post={post?.attributes} />
-                ))}
-            </div>
-        </div>
-    );
+    return <Outlet />;
 }
 export default Blog;
