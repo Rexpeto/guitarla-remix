3a6172f96398824fb375e993a7f7342d26370f52
Carlos Gallardo <Carlosgallardoparra@hotmail.com>
2023-03-11 23:12:53 -0400

    Componente de curso

---
+++ app/components/curso.jsx
@@ -0,0 +1,27 @@
+const Curso = ({ curso }) => {
+    const { titulo, contenido, imagen } = curso;
+    const url = imagen?.data?.attributes?.formats?.large?.url;
+
+    return (
+        <section className="curso">
+            <style jsx>{`
+                .curso {
+                    background-image: linear-gradient(
+                            to right,
+                            rgb(0 0 0 / 0.65),
+                            rgb(0 0 0 / 0.7)
+                        ),
+                        url(${url});
+                }
+            `}</style>
+            <div className="contenedor curso-grid">
+                <div className="contenido">
+                    <h2 className="heading">{titulo}</h2>
+                    <p className="texto">{contenido}</p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Curso;
