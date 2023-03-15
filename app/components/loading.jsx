9bca7f6d5f2ba28753b7e39b4cf33bda19b45fa8
Rexpeto <carlosgallardoparra@hotmail.com>
2023-03-15 15:53:29 -0400

    Componente de loading

---
+++ app/components/loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className="loader-container">
+            <span className="loader"></span>
+        </div>
+    );
+};
+
+export default Loading;
