b74ada0fd1c479d2d7d5140b5263d421f23d97f4
Rexpeto <carlosgallardoparra@hotmail.com>
2023-03-15 12:59:34 -0400

    Context actualizarCantidad

--- app/root.jsx
+++ app/root.jsx
@@ -58,12 +58,25 @@ export default function App() {
         }
     };
 
+    const actualizarCantidad = (guitarra) => {
+        const carritoActualizado = carrito.map((guitarraState) => {
+            if (guitarraState.id === guitarra.id) {
+                guitarraState.cantidad = guitarra.cantidad;
+            }
+
+            return guitarraState;
+        });
+
+        setCarrito(carritoActualizado);
+    };
+
     return (
         <Document>
             <Outlet
                 context={{
                     agregarCarrito,
                     carrito,
+                    actualizarCantidad,
                 }}
             />
         </Document>
